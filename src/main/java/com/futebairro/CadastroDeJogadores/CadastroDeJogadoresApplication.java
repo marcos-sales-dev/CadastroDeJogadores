6d1c1dbac9bbdb88167f68798cf1737aa36bf6c5
Marcos Sales <marcos.sales.dev.com>
2025-08-26 09:00:51 -0300

    first commit

---
+++ src/main/java/com/futebairro/CadastroDeJogadores/CadastroDeJogadoresApplication.java
@@ -0,0 +1,13 @@
+package com.futebairro.CadastroDeJogadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeJogadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeJogadoresApplication.class, args);
+	}
+
+}
